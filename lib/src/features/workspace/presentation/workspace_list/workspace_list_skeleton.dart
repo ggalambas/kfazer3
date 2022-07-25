801c166cd14c6265591efa83e3b925421174a8b9
agalambas <agalambas@gmail.com>
2022-07-25 17:40:28 +0100

    Merge branch 'master' of https://github.com/ggalambas/kfazer3

---
+++ lib/src/features/workspace/presentation/workspace_list/workspace_list_skeleton.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:kfazer3/src/common_widgets/responsive_center.dart';
+import 'package:kfazer3/src/utils/int_to_list.dart';
+import 'package:skeletons/skeletons.dart';
+import 'package:smart_space/smart_space.dart';
+
+class WorkspaceListSkeleton extends StatelessWidget {
+  const WorkspaceListSkeleton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        // workspace avatar diameter + padding + margin
+        const itemHeight = 64; // 40+16+8
+        final itemCount = (constraints.maxHeight / itemHeight).floor();
+        return ResponsiveCenter(
+          padding: EdgeInsets.all(kSpace),
+          child: Column(
+            children: [
+              ...itemCount.generateList().map(
+                    (_) => SkeletonItem(
+                      child: Card(
+                        child: SizedBox.fromSize(
+                          // workspace avatar diameter + padding
+                          size: const Size.fromHeight(56), // 40+16
+                        ),
+                      ),
+                    ),
+                  )
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
