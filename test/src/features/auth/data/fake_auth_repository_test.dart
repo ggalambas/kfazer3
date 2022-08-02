d5ccec7187bc35b4e014c976e8efc2ef4327e111
ggalambas <ggalambas@gmail.com>
2022-08-02 16:24:24 +0100

    auth repo tests

--- test/src/features/auth/data/fake_auth_repository_test.dart
+++ test/src/features/auth/data/fake_auth_repository_test.dart
@@ -16,13 +16,20 @@ void main() {
   FakeAuthRepository makeAuthRepository() =>
       FakeAuthRepository(addDelay: false);
   group('FakeAuthRepository', () {
-    test('currentUser is null', () {
+    test('currentUser is not null', () {
       final authRepository = makeAuthRepository();
+      expect(authRepository.currentUser, kTestUsers.first);
+      expect(authRepository.authStateChanges(), emits(kTestUsers.first));
+    });
+    test('currentUser is null after sign out', () async {
+      final authRepository = makeAuthRepository();
+      await authRepository.signOut();
       expect(authRepository.currentUser, null);
       expect(authRepository.authStateChanges(), emits(null));
     });
-    test('currentUser is still null after sending sms code', () {
+    test('currentUser is null after send sms code', () async {
       final authRepository = makeAuthRepository();
+      await authRepository.signOut();
       authRepository.sendSmsCode(testPhoneNumber);
       expect(authRepository.currentUser, null);
       expect(authRepository.authStateChanges(), emits(null));
@@ -30,51 +37,45 @@ void main() {
     test('currentUser is not null after sign in', () async {
       final authRepository = makeAuthRepository();
       final user = kTestUsers.first;
+      await authRepository.signOut();
       await authRepository.verifySmsCode(user.phoneNumber, '123456');
       expect(authRepository.currentUser, user);
       expect(authRepository.authStateChanges(), emits(user));
     });
     test('currentUser is null after first time sign in', () async {
       final authRepository = makeAuthRepository();
+      await authRepository.signOut();
       await authRepository.verifySmsCode(testPhoneNumber, '123456');
       expect(authRepository.currentUser, null);
       expect(authRepository.authStateChanges(), emits(null));
     });
     test('currentUser is not null after registration', () async {
       final authRepository = makeAuthRepository();
+      await authRepository.signOut();
       await authRepository.createUser(testPhoneNumber, testName);
       expect(authRepository.currentUser, testUser);
       expect(authRepository.authStateChanges(), emits(testUser));
     });
-    test('currentUser is null after sign out', () async {
-      final authRepository = makeAuthRepository();
-      await authRepository.createUser(testPhoneNumber, testName);
-      await authRepository.signOut();
-      expect(authRepository.currentUser, null);
-      expect(authRepository.authStateChanges(), emits(null));
-    });
     test('currentUser is null after delete', () async {
       final authRepository = makeAuthRepository();
-      await authRepository.createUser(testPhoneNumber, testName);
       await authRepository.deleteUser();
       expect(authRepository.currentUser, null);
       expect(authRepository.authStateChanges(), emits(null));
     });
     test('currentUser updates', () async {
       final authRepository = makeAuthRepository();
-      await authRepository.createUser(testPhoneNumber, testName);
-      final updatedUser = testUser
+      final updatedUser = kTestUsers.first
           .updatePhoneNumber(PhoneNumber('+351', '961234578'))
           .updateName('Jimbo');
       await authRepository.updateUser(updatedUser);
       expect(authRepository.currentUser, updatedUser);
       expect(authRepository.authStateChanges(), emits(updatedUser));
     });
-    test('sign in after dispose throws exception', () async {
+    test('sign out after dispose throws exception', () async {
       final authRepository = makeAuthRepository();
       authRepository.dispose();
       expect(
-        () => authRepository.createUser(testPhoneNumber, testName),
+        authRepository.signOut,
         throwsStateError,
       );
     });
