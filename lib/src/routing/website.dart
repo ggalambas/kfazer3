432aad709ce6a0c5ee99143e1d6b7cc1af91d2e5
ggalambas <ggalambas@gmail.com>
2022-08-02 14:33:05 +0100

    contacts link

--- lib/src/routing/website.dart
+++ lib/src/routing/website.dart
@@ -1,7 +1,7 @@
 /// External URI used in the app.
 class Website {
   static Uri get sheetTemplate => Uri.https('k-fazer.com', '/sheet-template');
-  static Uri get contactUs => Uri.https('k-fazer.com', '/contact-us');
+  static Uri get contactUs => Uri.https('k-fazer.com', '/#contactos');
   static Uri get policies => Uri.https('k-fazer.com', '/policies');
   static Uri get terms => Uri.https('k-fazer.com', '/policies/terms');
   static Uri get privacy => Uri.https('k-fazer.com', '/policies/privacy');
