e241c6e43f1df1e899e5ab58cd38e7aaab82217a
ggalambas <ggalambas@gmail.com>
2022-08-02 16:34:16 +0100

    ws repo tests

--- test/src/features/workspace/data/fake_workspace_repository_test.dart
+++ test/src/features/workspace/data/fake_workspace_repository_test.dart
@@ -2,6 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:kfazer3/src/constants/test_workspaces.dart';
 import 'package:kfazer3/src/features/workspace/data/fake_workspaces_repository.dart';
 import 'package:kfazer3/src/features/workspace/data/workspace_repository.dart';
+import 'package:kfazer3/src/features/workspace/domain/preferences.dart';
+import 'package:kfazer3/src/features/workspace/domain/updatable_workspace.dart';
 
 void main() {
   WorkspaceRepository makeWorkspaceRepository() =>
@@ -27,6 +29,38 @@ void main() {
       final workspaceRepository = makeWorkspaceRepository();
       expect(
         workspaceRepository.watchWorkspace('-1'),
+        emits(null),
+      );
+    });
+    test('workspace updates', () async {
+      final authRepository = makeWorkspaceRepository();
+      final workspace = kTestWorkspaces.first;
+      final updatedWorkspace = workspace
+          .updateTitle('New title')
+          .updateDescription('New description')
+          .updatePlan(WorkspacePlan.standard)
+          .updateMotivationalMessages([]);
+      await authRepository.updateWorkspace(updatedWorkspace);
+      expect(
+        authRepository.watchWorkspace(workspace.id),
+        emits(updatedWorkspace),
+      );
+    });
+    test('workspace disappears after delete', () async {
+      final authRepository = makeWorkspaceRepository();
+      final workspace = kTestWorkspaces.first;
+      await authRepository.deleteWorkspace(workspace.id);
+      expect(
+        authRepository.watchWorkspace(workspace.id),
+        emits(null),
+      );
+    });
+    test('workspace disappears after leave', () async {
+      final authRepository = makeWorkspaceRepository();
+      final workspace = kTestWorkspaces.first;
+      await authRepository.leaveWorkspace(workspace.id);
+      expect(
+        authRepository.watchWorkspace(workspace.id),
         emits(null),
       );
     });
