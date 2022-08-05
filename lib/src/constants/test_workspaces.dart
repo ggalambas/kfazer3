d071e7c5a58f1408195b23223d73bc091ea3f038
agalambas <agalambas@gmail.com>
2022-08-05 11:55:00 +0100

    getters on fake data

--- lib/src/constants/test_workspaces.dart
+++ lib/src/constants/test_workspaces.dart
@@ -1,14 +1,15 @@
 import 'package:kfazer3/src/features/workspace/domain/workspace.dart';
 
 /// Test workspaces to be used until a data source is implemented
-final kTestWorkspaces = List.generate(
+List<Workspace> get kTestWorkspaces => [..._kTestWorkspaces];
+final _kTestWorkspaces = List.generate(
   2,
   (i) => Workspace(
     id: '$i',
     title: 'Workspace $i',
     description:
         'A workspace made by him for the company x in the center of the world.',
-    motivationalMessages: kMotivationalMessages,
+    motivationalMessages: [...kMotivationalMessages],
   ),
 );
 
