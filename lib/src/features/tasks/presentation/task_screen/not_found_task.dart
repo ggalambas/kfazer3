a98446c7b44434f242d18d324b3fc43035d4987e
ggalambas <ggalambas@gmail.com>
2022-10-18 15:04:43 +0100

    task screen bottom bar

---
+++ lib/src/features/tasks/presentation/task_screen/not_found_task.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kfazer3/src/common_widgets/not_found_widget.dart';
+import 'package:kfazer3/src/localization/app_localizations_context.dart';
+
+class NotFoundTask extends ConsumerWidget {
+  const NotFoundTask({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Material(
+      child: NotFoundWidget(
+        message: context.loc.taskNotFound,
+      ),
+    );
+  }
+}
--- lib/src/features/tasks/presentation/task_screen/task_screen.dart
+++ lib/src/features/tasks/presentation/task_screen/task_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kfazer3/src/common_widgets/async_value_widget.dart';
-import 'package:kfazer3/src/common_widgets/not_found_widget.dart';
 import 'package:kfazer3/src/common_widgets/responsive_center.dart';
 import 'package:kfazer3/src/features/tasks/data/tasks_repository.dart';
 import 'package:kfazer3/src/features/tasks/domain/task.dart';
-import 'package:kfazer3/src/localization/app_localizations_context.dart';
-import 'package:kfazer3/src/utils/context_theme.dart';
+import 'package:kfazer3/src/features/tasks/presentation/task_screen/not_found_task.dart';
+import 'package:kfazer3/src/localization/string_hardcoded.dart';
 import 'package:smart_space/smart_space.dart';
+
+import 'task_details.dart';
 
 /// Shows the task page for a given task ID.
 class TaskScreen extends ConsumerWidget {
@@ -17,46 +18,48 @@ class TaskScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final taskValue = ref.watch(taskProvider(taskId));
-    return Scaffold(
-      appBar: AppBar(),
-      body: AsyncValueWidget<Task?>(
-        value: taskValue,
-        data: (task) => task == null
-            ? NotFoundWidget(message: context.loc.taskNotFound)
-            : CustomScrollView(
-                slivers: [
-                  ResponsiveSliverCenter(
-                    padding: EdgeInsets.all(kSpace),
-                    child: TaskDetails(task: task),
-                  ),
-                ],
+    return AsyncValueWidget<Task?>(
+      value: taskValue,
+      data: (task) {
+        if (task == null) return const NotFoundTask();
+        return Scaffold(
+          appBar: AppBar(),
+          body: CustomScrollView(
+            slivers: [
+              ResponsiveSliverCenter(
+                padding: EdgeInsets.all(kSpace),
+                child: TaskDetails(task: task),
               ),
-      ),
-    );
-  }
-}
-
-/// Shows all the task details along with actions to:
-/// - delegate
-/// - split
-/// - reject
-/// - mark as completed
-class TaskDetails extends StatelessWidget {
-  final Task task;
-  const TaskDetails({super.key, required this.task});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(kSpace),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Text(task.description, style: context.textTheme.titleLarge),
-          Space(),
-          Text(task.state.name),
-        ],
-      ),
+            ],
+          ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {}, //TODO Mark as completed
+            child: const Icon(Icons.check),
+          ),
+          bottomNavigationBar: BottomAppBar(
+            child: Row(
+              children: [
+                IconButton(
+                  tooltip: 'Delegate'.hardcoded,
+                  onPressed: () {}, //TODO Delegate task
+                  icon: const Icon(Icons.double_arrow),
+                ),
+                IconButton(
+                  tooltip: 'Split'.hardcoded,
+                  onPressed: () {}, //TODO Split task
+                  icon: const Icon(Icons.view_stream),
+                ),
+                IconButton(
+                  tooltip: 'Deny'.hardcoded,
+                  onPressed: () {}, //TODO Deny task
+                  icon: const Icon(Icons.cancel),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
