31f539f957013c74f4e4dfb06d9cab20e85be7e4
ggalambas <ggalambas@gmail.com>
2022-11-09 15:09:28 +0000

    user info dialog separatd from user avatar file

---
+++ lib/src/common_widgets/user_info_dialog.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kfazer3/src/features/auth/domain/app_user.dart';
+import 'package:kfazer3/src/localization/app_localizations_context.dart';
+import 'package:kfazer3/src/utils/context_theme.dart';
+import 'package:smart_space/smart_space.dart';
+
+import 'user_avatar.dart';
+
+class UserInfoDialog extends ConsumerWidget {
+  final AppUser user;
+  const UserInfoDialog(this.user, {super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return AlertDialog(
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Space(),
+          UserAvatar(
+            user,
+            radius: kSpace * 5,
+            dialogOnTap: false,
+          ),
+          Space(2),
+          Text(
+            user.name,
+            style: context.textTheme.titleLarge,
+          ),
+          Text(
+            user.phoneNumber.full,
+            style: context.textTheme.bodySmall,
+          ),
+        ],
+      ),
+      actions: [
+        TextButton(
+          onPressed: Navigator.of(context).pop,
+          child: Text(context.loc.ok),
+        ),
+      ],
+    );
+  }
+}
