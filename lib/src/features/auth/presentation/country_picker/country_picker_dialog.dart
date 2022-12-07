effe5cb7a5aa6db339daa0b81f769c2e2484433a
ggalambas <ggalambas@gmail.com>
2022-12-07 12:26:45 +0000

    country picker dialog separate classes to their own files

--- lib/src/features/auth/presentation/country_picker/country_picker_dialog.dart
+++ lib/src/features/auth/presentation/country_picker/country_picker_dialog.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:kfazer3/src/common_widgets/async_value_widget.dart';
 import 'package:kfazer3/src/constants/breakpoints.dart';
-import 'package:kfazer3/src/constants/constants.dart';
-import 'package:kfazer3/src/features/auth/data/country_repository.dart';
-import 'package:kfazer3/src/features/auth/domain/country.dart';
-import 'package:kfazer3/src/localization/localized_context.dart';
 import 'package:smart_space/smart_space.dart';
 
-import 'country_tile.dart';
+import 'country_list_view.dart';
+import 'country_search_field.dart';
 
 class CountryPickerDialog extends StatefulWidget {
   const CountryPickerDialog({super.key});
@@ -42,74 +37,6 @@ class _CountryPickerDialogState extends State<CountryPickerDialog> {
               query: searchController.text.trim().toLowerCase(),
             ),
           ),
-        );
-      },
-    );
-  }
-}
-
-class CountrySearchField extends StatelessWidget {
-  final TextEditingController controller;
-  const CountrySearchField({super.key, required this.controller});
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: kSpace * 2),
-        child: TextField(
-          controller: controller,
-          textInputAction: TextInputAction.search,
-          decoration: InputDecoration(
-            hintText: context.loc.searchHint,
-            suffixIcon: controller.text.isEmpty
-                ? const IconButton(
-                    iconSize: kSmallIconSize,
-                    onPressed: null,
-                    icon: Icon(Icons.search_outlined),
-                  )
-                : IconButton(
-                    tooltip: context.loc.clear,
-                    iconSize: kSmallIconSize,
-                    onPressed: () => controller.text = '',
-                    icon: const Icon(Icons.clear_outlined),
-                  ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class CountryListView extends ConsumerWidget {
-  final String query;
-  const CountryListView({super.key, this.query = ''});
-
-  List<Country> filteredCountries(List<Country> countryList) {
-    return countryList
-        .where((country) =>
-            country.name.toLowerCase().contains(query) ||
-            country.code.toLowerCase().contains(query) ||
-            country.phoneCode.toString().toLowerCase().contains(query))
-        .toList();
-  }
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final countryListValue = ref.watch(countryListFutureProvider);
-    return AsyncValueWidget<List<Country>>(
-      value: countryListValue,
-      loading: ListView.builder(
-        padding: EdgeInsets.only(top: kSpace),
-        itemBuilder: (context, i) => const LoadingCountryTile(),
-      ),
-      data: (countryList) {
-        final countries = filteredCountries(countryList);
-        return ListView.builder(
-          padding: EdgeInsets.only(top: kSpace),
-          itemCount: countries.length,
-          itemBuilder: (context, i) => const LoadingCountryTile(),
-          // CountryTile(countries[i]),
         );
       },
     );
